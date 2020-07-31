455131f513e4a615feca024ba1c615e955905290
Thong Nguyen <bathong170698@gmail.com>
2020-07-31 16:32:16 -0400

    Add simple analytics script

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,13 +23,6 @@ module.exports = {
         path: `${__dirname}/content/`
       }
     },
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: 'posts',
-    //     path: `${__dirname}/content/posts`
-    //   }
-    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
